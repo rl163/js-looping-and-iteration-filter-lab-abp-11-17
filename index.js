0414ef88d6523a2804dee1890f7b59d20b6facd5
Ryan Lee <ryanlee91@gmail.com>
2017-11-18 11:27:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Code your solution in this file
 function findMatching(drivers, name){
   return drivers.filter(function(driver){
-    if(name.toLowerCase === driver.toLowerCase){
+    if(name.toLowerCase() === driver.toLowerCase()){
       return true
     } else {
       return false
     }
   })
+
+  return matchingDrivers;
 }
