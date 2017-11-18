f04f5d95200894ac7a784c64aabed05eb1884fda
Ryan Lee <ryanlee91@gmail.com>
2017-11-18 11:28:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, name){
-  return drivers.filter(function(driver){
+  return matchingDrivers = drivers.filter(function(driver){
     if(name.toLowerCase() === driver.toLowerCase()){
       return true
     } else {
