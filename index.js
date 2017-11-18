c75465497c31ed36d942e79f00d1ca0147a364d2
Ryan Lee <ryanlee91@gmail.com>
2017-11-18 11:25:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file
+function findMatching(drivers, name){
+  drivers.filter(function(driver){
+    if(name === driver){
+      return true
+    } else {
+      return false
+    }
+  })
+}
